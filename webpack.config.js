ccbae7c7a333ef698900e2d236fb16f64f8e82c7
Tony Rivera <tonyrivera@users.noreply.github.com>
2019-01-09 12:42:03 -0800

    Add boilerpoint webpack babel file/configs

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    context: __dirname + "/src",
+    entry: "./app.js",
+
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: require.resolve('babel-loader')
+            }
+          }
+        ]
+      },
+
+    output: {
+      filename: "bundle.js",
+      path: __dirname + "/build",
+    },
+  }
