946bdfa496b60a174af8709e195173304eb0c74c
Tony Rivera <tonyrivera@users.noreply.github.com>
2019-01-09 16:05:51 -0800

    add nav bar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react';
 class Nav extends Component {
     render(){
         return(
-            <div>Nav Bar</div>
+            <nav>
+                <img src="https://news.ycombinator.com/y18.gif" class="logo" />
+                <ul>
+                    <li><strong><a href="https://news.ycombinator.com/news">Hacker News</a></strong></li>
+                    <li>new</li>
+                    <li>comments</li>
+                    <li>show</li>
+                    <li>ask</li>
+                    <li>jobs</li>
+                    <li>submit</li>
+                    <li id="login">login</li>
+                </ul>
+            </nav>
         )
     }
 }
